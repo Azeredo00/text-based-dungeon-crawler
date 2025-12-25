47823d3641b2472a2f356de4a3f6df073fe3fb60
Azeredo00 <161282466+Azeredo00@users.noreply.github.com>
2025-12-25 03:44:25 +0000

    skeleton generator

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 typedef struct{
-    int id;
-    int adjacent_rooms[4];
+    int adjacent_rooms[4]; //array positions are, respectively: north, east, south, west
     int enemies_inside;
     int money_pile;
 }rooms;
 
 typedef struct{
-    int id;
     int health_points;
     int armor_class;
     int damage_points;
@@ -18,9 +16,30 @@ typedef struct{
     int inteligence;
     int wisdom;
     int charisma;
-}enemies;
+}sheets;
 
 int main(){
-    printf("a");
+    srand(time(NULL));
+    int skeleton_quantity;
+    scanf("%d", &skeleton_quantity);
+
+    //this line creates an array of pointers pointing to a memory space that holds a 'sheets' struct
+    sheets **skeleton = malloc(skeleton_quantity * sizeof(sheets *));
+    for(int i = 0; i < skeleton_quantity; i++){
+        
+        //this line allocates memory for each struct 'sheets' and assigns the pointer to the array
+        skeleton[i] = malloc(sizeof(sheets));
+        
+        //example of assigning values to the struct members
+        skeleton[i]->health_points = rand()%8+1 + rand()%8+1 + 4; //2d8 roll + 4
+        skeleton[i]->armor_class = 13;
+        skeleton[i]->damage_points = 5; //or 1d6+2
+        skeleton[i]->strength = rand()%20+1; //random value between 1 and 20
+        skeleton[i]->dexterity = rand()%20+1;
+        skeleton[i]->constitution = rand()%20+1;
+        skeleton[i]->inteligence = rand()%20+1;
+        skeleton[i]->wisdom = rand()%20+1;
+        skeleton[i]->charisma = rand()%20+1;
+    }
     return 0;
 }
