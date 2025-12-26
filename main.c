65b5a16a04b991c870f8593d0a2e7617cfa8b7b5
Azeredo00 <161282466+Azeredo00@users.noreply.github.com>
2025-12-26 05:03:05 +0000

    basic battle generator done

--- main.c
+++ main.c
@@ -6,9 +6,13 @@ typedef struct{
     int adjacent_rooms[4]; //array positions are, respectively: north, east, south, west
     int enemies_inside;
     int money_pile;
-}rooms;
+    int has_enemy;
+}Room;
 
 typedef struct{
+    int health_dice_quantity;
+    int health_dice;
+    int health_bonus;
     int health_points;
     int armor_class;
     int damage_dice_quantity;
@@ -20,42 +24,50 @@ typedef struct{
     int inteligence;
     int wisdom;
     int charisma;
-}sheets;
+}Sheet;
 
-int main(){
-    sheets player={20, 15, 1, 8, 3, 15, 14, 13, 12, 10, 8};
-    srand(time(NULL));
-    int enemies_quantity, id, roll_counter, damage_dealt, enemies_defeated;
+Sheet player={3, 10, 6, 1, 15, 2, 6, 3, 16, 12, 14, 10, 8}; //player character sheet
+Sheet monsters[3]={ 
+    {2, 8, 4, 0, 13, 1, 6, 2, 10, 14, 15, 6, 8, 5}, //skeleton
+    {3, 8, 9, 0, 8, 1, 6, 1, 13, 6, 16, 3, 6, 5}, //zombie
+    {2, 6, 0, 0, 15, 1, 6, 2, 8, 14, 10, 10, 8, 8} //goblin
+};
 
-    enemies_quantity = 3;
+//created a battle function that takes, as a parameter, a pointer to a Sheet structs
+void battle(){
+
+    int enemies_quantity, id, roll_counter, damage_dealt, enemies_defeated, type_of_enemy;
+
+    enemies_quantity = rand()%7+1;
     enemies_defeated = 0;
+    type_of_enemy = rand()%3; //0 = skeleton, 1 = zombie, 2 = goblin
 
-    //this line creates a pointer to pointers, an array of pointers, pointing to a memory space that holds a 'sheets' struct
-    sheets **enemies = malloc(enemies_quantity * sizeof(sheets *));    
+    //creates a pointer to pointers, an array of pointers, pointing to a memory space that will hold a 'Sheet' struct
+    //because the number of enemies is dynamic, we allocate memory for the array of pointers
+    Sheet **enemies = malloc(enemies_quantity * sizeof(Sheet *));    
 
-    for(id=0; id<3; id++){        
-        //this line allocates memory for each struct 'sheets' and assigns the pointer to the array
-        enemies[id] = malloc(sizeof(sheets));
+    for(id=0; id<enemies_quantity; id++){
+
+        enemies[id] = malloc(sizeof(Sheet)); //allocates memory for each struct 'Sheet' and assigns the pointer to the array
+        *enemies[id] = monsters[type_of_enemy]; //copies the monster stats to the allocated memory
+
+        for(roll_counter=1; roll_counter<=enemies[id]->health_dice_quantity; roll_counter++){ //total health points calculation
+            enemies[id]->health_points += rand()%enemies[id]->health_dice+1;
+        }
+        enemies[id]->health_points += enemies[id]->health_bonus;
         
-        sheets skeleton = {rand()%8+1 + rand()%8+1 + 4, 13, 1, 6, 2, 10, 14, 15, 6, 8, 5};
-        *enemies[id] = skeleton;
     }
-    for(id=0; id<3; id++){
-        
-        //this line allocates memory for each struct 'sheets' and assigns the pointer to the array
-        enemies[id] = malloc(sizeof(sheets));
+    printf("A battle has started! There are a horde of %d enemies of type %d!\n", enemies_quantity, type_of_enemy);
+    while(player.health_points>0 && enemies_defeated<enemies_quantity){
 
-        sheets zombie = {rand()%8+1 + rand()%8+1 + rand()%8+1 + 9, 8, 1, 6, 1, 13, 6, 16, 3, 6, 5};
-        *enemies[id] = zombie;
-    }
-
-    while( player.health_points>0 && enemies_defeated<enemies_quantity ){
         printf("\n-- New Turn --\n");
         printf("Player HP: %d\n", player.health_points);
-        for(id=0; id<3; id++){ //player attacks each skeleton in the room
+
+        for(id=0; id<enemies_quantity; id++){ //player attacks each enemy in the room
+
             damage_dealt = 0; //each new battle, a new damage value is dealt
-            printf("Skeleton %d HP: %d\n", id+1, enemies[id]->health_points);
-            if(enemies[id]->health_points > 0){ //only attack if the skeleton is alive
+            printf("Enemy %d HP: %d\n", id+1, enemies[id]->health_points);
+            if(enemies[id]->health_points > 0){ //only attack if the enemy is alive
 
                 for(roll_counter=1; roll_counter<=player.damage_dice_quantity; roll_counter++){
                     damage_dealt += rand()%player.damage_dice+1;
@@ -64,26 +76,30 @@ int main(){
                     }
                 }
 
-                printf("Player deals %d damage to Skeleton %d\n", damage_dealt, id+1);
+                printf("Player deals %d damage to the enemy %d\n", damage_dealt, id+1);
 
-                if(damage_dealt >= enemies[id]->health_points){ //if the skeleton is defeated
+                if(damage_dealt >= enemies[id]->health_points){ //if the enemy is defeated
                     enemies[id]->health_points = 0;
                     enemies_defeated++;
-                    printf("Skeleton %d defeated!\n", id+1);
+                    printf("Enemy %d defeated!\n", id+1);
                 }else{
                     enemies[id]->health_points -= damage_dealt;
                 }
             }else{
-                printf("Skeleton %d is already defeated\n", id+1);
+                printf("Enemy %d is already defeated\n", id+1);
             }
         }
     }
-    //only when the combat ends, free the memory allocated
-    printf("Combat ended with %d Enemies defeated\n", enemies_defeated);
+    //only when the battle ends, free the memory allocated
+    printf("Battle ended with %d Enemies defeated\n", enemies_defeated);
     for(id=0; id<3; id++){
         free(enemies[id]);
     }
     free(enemies);
+}
 
+int main(){
+    srand(time(NULL));
+    battle();
     return 0;
 }
