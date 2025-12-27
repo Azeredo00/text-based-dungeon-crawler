58eabe1d77eff957b9bc38bbfab8aad7374ae72b
Azeredo00 <161282466+Azeredo00@users.noreply.github.com>
2025-12-27 21:35:13 +0000

    rework on the structs and the player creator

--- main.c
+++ main.c
@@ -10,36 +10,62 @@ typedef struct{
 }Room;
 
 typedef struct{
-    Class Class;
     int health_points;
     int armor_class;
-    /*
-    int damage_dice_quantity;
-    int damage_dice;
-    int damage_bonus;
-    */
+    Weapon weapon;
     int damage;
     int strength;
     int dexterity;
     int constitution;
-    int inteligence;
+    int intelligence;
     int wisdom;
     int charisma;
-}Sheet;
+}Base;
+
+typedef struct{
+    Base basic_stats;
+    int health_dice_quantity;
+    int health_dice;
+    int health_bonus;
+}Monster;
+
+typedef struct{
+    //players do not need to have a copy of the class and race
+    //since all the players of the same class and race would share the same stats
+    //we can just have a pointer instead of copies of the structs
+    Class *class;
+    Base basic_stats;
+    Race *race;
+}Person;
 
 //the health points value here does not matter, because they will be calculated at the start of the battle
 //because they are random, and random values can not be assigned at compile time
-Sheet players[4]; //array of 4 players
-Sheet monsters[3]={ 
-    {2, 8, 4, 0, 13, 1, 6, 2, 10, 14, 15, 6, 8, 5}, //skeleton
-    {3, 8, 9, 0, 8, 1, 6, 1, 13, 6, 16, 3, 6, 5}, //zombie
-    {2, 6, 0, 0, 15, 1, 6, 2, 8, 14, 10, 10, 8, 8} //goblin
+Person players[4]; //array of 4 players
+Monster monsters[3]={
+    /* 
+    {2, 8, 4, 0, 0, 13, 0, 1, 6, 2, 10, 14, 15, 6, 8, 5}, //skeleton
+    {3, 8, 9, 0, 0, 8, 0, 1, 6, 1, 13, 6, 16, 3, 6, 5}, //zombie
+    {2, 6, 0, 0, 0, 15, 0, 1, 6, 2, 8, 14, 10, 10, 8, 8} //goblin
+    */    
 };
+
+typedef struct{
+    int damage_dice_quantity;
+    int damage_dice;
+    int damage_bonus;
+}Weapon;
+
+Weapon weapons[3]={ //0 = sword, 1 = bow, 2 = staff
+    {1, 8, 2}, //sword
+    {1, 6, 1}, //bow
+    {1, 4, 3}  //staff
+};
+
 typedef struct{
     int strength_bonus;
     int dexterity_bonus;
     int constitution_bonus;
-    int inteligence_bonus;
+    int intelligence_bonus;
     int wisdom_bonus;
     int charisma_bonus;
 }Race;
@@ -56,15 +82,13 @@ typedef struct{
     int health_bonus;
 }Class;
 
-Class classes[3]={ //0 = warrior, 1 = rogue, 2 = mage
+Class classes[3]={ //0=warrior, 1=rogue, 2=mage
     {1, 12, 12}, //warrior
-    {1, 8, 8}, //rogue
-    {1, 6, 6},  //mage
-    {0, 0, 0}  //enemy
+    {1, 8, 8}, //rogue  
+    {1, 6, 6}  //mage
 };
 //created a battle function that takes, as a parameter, a pointer to a Sheet structs
 void battle(){
-
     int enemies_quantity, id, roll_counter, damage_dealt, enemies_defeated, type_of_enemy;
 
     enemies_quantity = rand()%7+1;
@@ -78,7 +102,7 @@ void battle(){
     for(id=0; id<enemies_quantity; id++){
 
         enemies[id] = malloc(sizeof(Sheet)); //allocates memory for each struct 'Sheet' and assigns the pointer to the array
-        *enemies[id] = monster_templates[type_of_enemy]; //copies the monster stats to the allocated memory
+        *enemies[id] = monsters[type_of_enemy]; //copies the monster stats to the allocated memory
 
         for(roll_counter=1; roll_counter<=enemies[id]->health_dice_quantity; roll_counter++){ //total health points calculation
             enemies[id]->health_points += rand()%enemies[id]->health_dice+1;
@@ -87,10 +111,10 @@ void battle(){
         
     }
     printf("A battle has started! There are a horde of %d enemies of type %d!\n", enemies_quantity, type_of_enemy);
-    while(player.health_points>0 && enemies_defeated<enemies_quantity){
+    while(players[id].basic_stats.health_points>0 && enemies_defeated<enemies_quantity){
 
         printf("\n-- New Turn --\n");
-        printf("Player HP: %d\n", player.health_points);
+        printf("Player HP: %d\n", players[id].basic_stats.health_points);
 
         for(id=0; id<enemies_quantity; id++){ //player attacks each enemy in the room
 
@@ -98,10 +122,10 @@ void battle(){
             printf("Enemy %d HP: %d\n", id+1, enemies[id]->health_points);
             if(enemies[id]->health_points > 0){ //only attack if the enemy is alive
 
-                for(roll_counter=1; roll_counter<=player.damage_dice_quantity; roll_counter++){
-                    damage_dealt += rand()%player.damage_dice+1;
-                    if(roll_counter == player.damage_dice_quantity){
-                        damage_dealt += player.damage_bonus;
+                for(roll_counter=1; roll_counter<=players[id].basic_stats.weapon.damage_dice_quantity; roll_counter++){
+                    damage_dealt += rand()%players[id].basic_stats.weapon.damage_dice+1;
+                    if(roll_counter == players[id].basic_stats.weapon.damage_dice_quantity){
+                        damage_dealt += players[id].basic_stats.weapon.damage_bonus;
                     }
                 }
 
@@ -129,12 +153,12 @@ void battle(){
 
 int main(){
     srand(time(NULL));
-    int selected_class, health_given, armor_class_given, id, attributes[6], choice;
+    int id, id2, attributes[6], choice, roll_counter;
 
     //character creation
     for(id=0; id<4; id++){
-        attributes[6]={rand()%20+1, rand()%20+1, rand()%20+1, rand()%20+1, rand()%20+1, rand()%20+1}; //ability scores rolls
-        printf("Attributes rolled: ");
+        attributes[6] = {rand()%20+1, rand()%20+1, rand()%20+1, rand()%20+1, rand()%20+1, rand()%20+1}; //ability scores rolls
+        printf("Dice rolls: ");
         for(choice=0; choice<6; choice++){
             printf("%d", attributes[choice]);
             if(choice<5){
@@ -144,33 +168,50 @@ int main(){
             }
         }
         printf("Distribute attributes for player %d:\n", id+1);
-        for(id=0; id<6; id++){
+        for(id2=0; id2<6; id2++){
+            printf("Choose the attribute to assign the roll value of %d: ", attributes[id2]);
+            printf("0 = strength, 1 = dexterity, 2 = constitution, 3 = intelligence, 4 = wisdom, 5 = charisma\n");
+            scanf("%d", &choice);
             switch(choice){
                 case 0:
+                    players[id].basic_stats.strength = attributes[id2];
                     break;
                 case 1:
+                    players[id].basic_stats.dexterity = attributes[id2];
                     break;
                 case 2:
+                    players[id].basic_stats.constitution = attributes[id2];
                     break;
                 case 3:
+                    players[id].basic_stats.intelligence = attributes[id2];
                     break;
                 case 4:
+                    players[id].basic_stats.
+                    wisdom = attributes[id2];
                     break;
                 case 5:
+                    players[id].basic_stats.charisma = attributes[id2];
                     break;
             }
         }
-        selected_class=rand()%3; //except the enemy class
-        health_given = rand()%classes[selected_class].health_dice + 1 + classes[selected_class].health_bonus; //initial health points roll
-        armor_class_given = 10 + players[id].dexterity; //initial armor class calculation
-        players[id]={
-            classes[selected_class],
-            health_given,
-            0,
-            0,
-            0,
-            0,
-            0, 0, 0, 0, 0, 0}; //attributes
+        players[id].class = &classes[rand()%3]; //except the enemy class
+        players[id].race = &races[rand()%3];
+        players[id].basic_stats.weapon = weapons[rand()%3];
+        players[id].basic_stats.health_points = rand()%(players[id].class->health_dice + 1) + players[id].class->health_bonus; //initial health points roll
+        players[id].basic_stats.armor_class = 10 + players[id].basic_stats.dexterity; //initial armor class calculation
+        players[id].basic_stats.damage = 0;
+        for(roll_counter=1; roll_counter<=players[id].basic_stats.weapon.damage_dice_quantity; roll_counter++){ //damage calculation
+            players[id].basic_stats.damage += rand()%(players[id].basic_stats.weapon.damage_dice + 1);
+            if(roll_counter == players[id].basic_stats.weapon.damage_dice_quantity){
+                players[id].basic_stats.damage += players[id].basic_stats.weapon.damage_bonus;
+            }
+        }
+        players[id].basic_stats.strength += players[id].race->strength_bonus;
+        players[id].basic_stats.dexterity += players[id].race->dexterity_bonus;
+        players[id].basic_stats.constitution += players[id].race->constitution_bonus;
+        players[id].basic_stats.intelligence += players[id].race->intelligence_bonus;
+        players[id].basic_stats.wisdom += players[id].race->wisdom_bonus;
+        players[id].basic_stats.charisma += players[id].race->charisma_bonus;
     };
     int roll_counter;
     battle();
